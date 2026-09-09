e26219fee1114d6ce9e968c1aa365878364749ad
Aaron Frawley <Aaron.Frawley01@student.wandw.ac.nz>
2026-09-09 12:26:36 +1200

    commit initial

---
+++ Security and Testing/Security and Testing For Trap Project/Security and Testing For Trap Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Security_and_Testing_For_Trap_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
